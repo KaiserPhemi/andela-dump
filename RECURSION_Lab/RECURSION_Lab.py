3cfd7ea3a911d8407c6ecb44e930e6f612adf1cb
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2016-09-16 23:55:18 +0100

    movie lab added

--- RECURSION_Lab/RECURSION_Lab.py
+++ RECURSION_Lab/RECURSION_Lab.py
@@ -2,50 +2,98 @@
 # 	try:
 # 		if (times <= 0):
 # 			return []
-# 		return [data] * times
-
-# 	except ValueError:
-# 		return 'The provided input is invalid'
+# 		else:
+# 			result = []
+# 			for i in range(1, times+1):
+# 				result.append(data)
+# 		return result
+		
+# 	except ValueError, e:
+# 		raise e
 
 # def replicate_recur(times, data):
 # 	try:
 # 		if (times <= 0):
 # 			return []
-# 		return [data] + replicate_recur(times-1, data)
+# 		else:
+# 			return [data] + replicate_recur(times-1, data)
+		
+# 	except ValueError, e:
+# 		raise e
+
+# def replicate_iter(times, data):
+# 	if (times <= 0):
+# 		return []
+# 	else:
+# 		result = []
+# 		for i in range(1, times+1):
+# 			result.append(data)
+# 		return result
+
+# 	raise ValueError('Invalid input')
+
+# def replicate_recur(times, data):
+# 	if (times <= 0):
+# 		return []
+# 	return [data] + replicate_recur(times-1, data)
 	
-# 	except ValueError:
-# 		return 'The provided input is invalid'
+# 	raise ValueError('Invalid input')
 
 # def replicate_iter(times, data):
 # 	if (times <= 0):
 # 		return []
 # 	else:
 # 		return [data] * times
-# 	raise ValueError("Invalid input")
+# 	raise ValueError('Invalid input')
 
 # def replicate_recur(times, data):
 # 	if (times<= 0):
 # 		return []
 # 	else:
 # 		return [data] + replicate_recur(times-1, data)
-# 	raise ValueError("Invalid input")
+# 	raise ValueError('Invalid input')
 
+# def replicate_iter(times, data):
+# 	if (times <= 0):
+# 		return []
+# 	elif(times > 0):
+# 		result = []
+# 		for i in range (times+1):
+# 			result.insert(i,data)
+# 		return result
+# 	else:
+#   		raise ValueError("Invalid input")
+	
+# def replicate_recur(times, data):
+# 	if (times <= 0):
+# 		return []
+# 	else:
+# 		return [data] + replicate_recur(times-1, data)
+	
 def replicate_iter(times, data):
-	if (times <= 0):
-		return []
-	elif(times > 0):
-		result = []
-		for i in range (times+1):
-			result.insert(i,data)
+	try:
+		if (times <= 0):
+			return []
+		else:
+			result = []
+			for i in range(1, times+1):
+				result.append(data)
 		return result
-	else:
-  		raise ValueError("Invalid input")
-	
+		
+	except (TypeError, AttributeError):
+		raise ValueError()
+
 def replicate_recur(times, data):
-	if (times <= 0):
-		return []
-	else:
-		return [data] + replicate_recur(times-1, data)
-	
-print(replicate_iter(10,[]))
-print(replicate_recur([],"kaiser"))
+	try:
+		if (times <= 0):
+			return []
+		else:
+			return [data] + replicate_recur(times-1, data)
+		raise ValueError('This is manually raised ValueError')
+		
+	except (TypeError, AttributeError):
+		raise ValueError()
+
+
+print(replicate_iter(1,[]))
+print(replicate_recur([],1))
